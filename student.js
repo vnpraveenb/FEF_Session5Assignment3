29bec033324ac18cc67a3379beef553116c57f08
Praveen Balijepalli <vnpraveenb@gmail.com>
2017-11-21 23:23:24 +0530

    Final Commit

--- student.js
+++ student.js
@@ -10,7 +10,7 @@ document.write("DOB: "  + studentDetails.DOB +" <br>");
 document.write("Skills: "  + studentDetails.skills +" <br> ");
 
 
-document.write("<br> Added Address and printing the Address <br><hr>");
+document.write("<br> Added Address and printing the Address <br> <br>");
 
 // Adding the Address Property to the studentDetails Object
 studentDetails.address = {state:"Andhra Pradesh", city:"Hyderabad",country: "India", pincode: 123456};
@@ -18,7 +18,7 @@ studentDetails.address = {state:"Andhra Pradesh", city:"Hyderabad",country: "Ind
 document.write("Address: <br> "  + "&nbsp;&nbsp;State: " + studentDetails.address.state +" <br>" + "&nbsp;&nbsp;City: " + studentDetails.address.city +" <br>" + "&nbsp;&nbsp;Country: " + studentDetails.address.country +" <br>" + "&nbsp;&nbsp;Pincode: " + studentDetails.address.pincode +" <br>");
 
 
-document.write("<br> Added EyeColor and Mother's Name and printing the them <br> <hr>");
+document.write("<br> Added EyeColor and Mother's Name and printing the them <br> <br>");
 
 // Adding the EyeColor Property to the studentDetails Object
 studentDetails.eyeColor= "Black";
@@ -30,13 +30,13 @@ document.write("EyeColor: "  + studentDetails.eyeColor +" <br>");
 document.write("Mother's Name: "  + studentDetails.motherName +" <br>");
 
 
-document.write("<br> Deleting Student EyeColor and printing the Object Details <br> ");
+document.write("<br><br>Deleting Student EyeColor and printing the Object Details <br> ");
 
 // Deleting the EyeColor Property of the studentDetails Object
 delete studentDetails.eyeColor;
 
 
-document.write("<br> Printing the details of the student object <br><hr>");
+document.write("<br><br> Printing the details of the student object <br><hr>");
 
 // Prints details of the studentDetails Object after deleting the EyeColor Property
 document.write("Name: "  + studentDetails.studentName +" <br>");
